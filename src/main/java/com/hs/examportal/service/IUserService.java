a1313e59a26d07c0e874ff7628a42df478fafe20
mdhasmatnoorani <mdhasmatnoorani@gmail.com>
2024-08-07 00:21:44 +0530

    Added code for create user

--- src/main/java/com/hs/examportal/service/IUserService.java
+++ src/main/java/com/hs/examportal/service/IUserService.java
@@ -1,4 +1,8 @@
 package com.hs.examportal.service;
 
+import com.hs.examportal.entity.User;
+
 public interface IUserService {
+
+    public User createUser(User user);
 }
--- src/main/java/com/hs/examportal/service/impl/UserServiceImpl.java
+++ src/main/java/com/hs/examportal/service/impl/UserServiceImpl.java
@@ -1,4 +1,20 @@
 package com.hs.examportal.service.impl;
 
-public class UserServiceImpl {
+import com.hs.examportal.entity.User;
+import com.hs.examportal.repository.UserRepository;
+import com.hs.examportal.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements IUserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User createUser(User user) {
+        User savedUser = userRepository.save(user);
+        return savedUser;
+    }
 }
